3c992c8dc6af0ad085fc0df0e920eb6be63d4dfb
Andrew Kirch <trelane@trelane.net>
2015-03-24 13:03:25 -0400

    template fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.trelane.Postfix"
-VERSION = "1.0.0"
+VERSION = "1.0.3"
 AUTHOR = "Andrew D Kirch"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.trelane']
